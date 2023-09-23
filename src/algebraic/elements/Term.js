5cb4bda3dbaae8cb2ad0349ae7ccc5c108d1b26c
JeissonR115 <jeissonfeliper@gmail.com>
2023-09-23 01:20:49 -0500

    add derivative

--- src/algebraic/elements/Term.js
+++ src/algebraic/elements/Term.js
@@ -5,4 +5,16 @@ export class Term {
         this.coefficient = coefficient;
         this.literalPart = variableList;
     }
+    derive(){
+        const newTerm = new Term()
+        if(this.literalPart[0]){
+
+            const variable = this.literalPart[0];
+            console.log(variable)
+            newTerm.coefficient = new Fraction(this.coefficient.num() * variable.exponent) ;
+            newTerm.literalPart = [new Variable(variable.letter,variable.letter?variable.exponent -1 :0)]
+        }
+
+        return newTerm;
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,36 @@
 import {extractAlgebraicExpression} from "./algebraic/algebraicExtractor.js";
 import {printExpression} from "./algebraic/algebraicPrinter.js";
 import {AlgebraicCalculator} from "./algebraic/algebraicCalculator.js";
-
-
+const radioInputList = document.querySelectorAll(".input-radio")
 const calculatorInput = document.querySelector(".calculator__input");
-const calculatorButton = document.querySelector(".calculator__button");
 const output =document.querySelector(".calculator__output");
 calculatorInput.addEventListener("input",() => {
     output.innerHTML = "";
     const expression = calculatorInput.value;
     const polynomials = extractAlgebraicExpression(expression);
     const sum = AlgebraicCalculator.summer(polynomials);
-    output.append(printExpression(sum,"listPolynomial  "))
+    //output.append(printExpression(sum,"listPolynomial  "))
+    output.append(printExpression(polynomials.derive(),"listPolynomial  "))
+})
+
+radioInputList.forEach(radioInput =>{
+    radioInput.addEventListener("click",()=>{
+        output.innerHTML = "";
+        const expression = calculatorInput.value;
+        const polynomial = extractAlgebraicExpression(expression);
+        let element;
+        switch (radioInput.id){
+            case "derivative-radio":
+                element =printExpression(polynomial.derive(),"derive-term")
+                break;
+            case "integral-radio":
+                element= printExpression(polynomial,'integral-term')
+                break;
+            case "default-radio":
+                element= printExpression(polynomial,'term')
+                break
+            default:console.log("error")
+        }
+        output.append(element)
+    })
 })
